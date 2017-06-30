8cb5676dd33d367f9776ccb23e4910a503d4c033
Connor Kelly <connor.r.kelly@gmail.com>
2017-06-29 23:30:36 -0500

    Add latest java_home

--- env/localpath.sh
+++ env/localpath.sh
@@ -2,7 +2,8 @@
 
 # User specific environment and startup programs
 
-PATH=$PATH:$HOME/.local/bin:$GOPATH/bin:$HOME/bin
+JAVA_HOME=/usr/java/jdk1.8.0_131
+PATH=$PATH:$JAVA_HOME/bin:$HOME/.local/bin:$GOPATH/bin:$HOME/bin
 
 export PATH
 
