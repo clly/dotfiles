da2024930df1339ce7cabbad41bf53b8dbd9cea3
Connor Kelly <connor@indeed.com>
2017-10-31 12:46:58 -0500

    Updates -- whoops

--- env/localpath.sh
+++ env/localpath.sh
@@ -2,7 +2,7 @@
 
 # User specific environment and startup programs
 
-JAVA_HOME=/usr/java/current
+JAVA_HOME=/usr/java/default
 PATH=$PATH:$JAVA_HOME/bin:$HOME/.local/bin:$GOPATH/bin:$HOME/bin
 
 export PATH
