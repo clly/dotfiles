453ad3cd4487f16df9793a63e687bfed540683e8
Connor Kelly <connor.r.kelly@gmail.com>
2018-06-03 16:21:29 -0500

    Set gpg to be gpg2

--- alias/alias.sh
+++ alias/alias.sh
@@ -5,3 +5,4 @@ alias ll='ls -l --time-style=long-iso'
 alias datetimestamp='date +%Y%m%d%H%M%S'
 alias srestart='sudo supervisorctl restart'
 alias fetch-root-servers='curl -o root.hints https://www.internic.net/domain/named.cache'
+alias gpg=gpg2
