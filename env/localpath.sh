14f0eafabb51db7ebc400a5c2ebf8eac8b64128b
Connor Kelly <connor.r.kelly@gmail.com>
2018-10-11 12:06:13 -0500

    Create function to deduplicate PATH variable

--- env/localpath.sh
+++ env/localpath.sh
@@ -13,3 +13,27 @@ PATH=$PATH:$JAVA_HOME/bin:$HOME/bin
 
 export PATH
 
+function dedupe_path() {
+    declare -A dedupe
+    declare -a newpath
+    local p=""
+    # set IFS to : for easy loops
+    OLD_IFS=$IFS
+    IFS=':'
+    for i in $PATH; do
+        if [[ ! ${dedupe[$i]+_} ]]; then
+            dedupe[$i]=1
+            newpath+=( $i )
+        fi
+    done
+    IFS=$OLD_IFS
+    # loop over deduplicated path to build new loop
+    for k in "${newpath[@]}"; do
+        p="${p}:${k}"
+    done
+    cut -f2- -d: <<< $p
+    unset dedupe
+    unset newpath
+    unset IFS
+    unset OLD_IFS
+}
