4e807e496cde66a02ad31b6d5bc463327be094ad
Connor Kelly <connor.r.kelly@gmail.com>
2018-10-11 16:09:11 -0500

    Create function to switch go versions

--- env/go.sh
+++ env/go.sh
@@ -7,3 +7,42 @@ export GOPATH=$HOME/go
 
 PATH=$GOROOT/bin:$PATH:$GOPATH/bin
 export PATH
+
+function goSwitch() {
+    set -x
+    if [[ -z $1 ]]; then
+        return 1
+    fi
+    local switchedGo=$1
+    local goSourcePath="/usr/go/go${switchedGo}.linux-amd64"
+    if [[ -d "/usr/go/go${switchedGo}.linux-amd64" ]]; then
+        declare -A dedupe
+        declare -a newpath
+        local p=""
+        IFS=':'
+        for i in $PATH; do
+            if [[ $i == "${GOROOT}/bin" ]]; then
+                if [[ ! ${dedupe["${goSourcePath}/bin"]+_} ]]; then
+                    echo "Adding go ${switchedGo} to PATH"
+                    dedupe["${goSourcePath}/bin"]=1
+                    newpath+=( "${goSourcePath}/bin" )
+                fi
+            else
+                if [[ ! ${dedupe[$i]+_} ]]; then
+                    dedupe[$i]=1
+                    newpath+=( $i )
+                fi
+            fi
+        done
+        # loop over deduplicated path to build new loop
+        for k in "${newpath[@]}"; do
+            p="${p}:${k}"
+        done
+        echo $p
+        export PATH=$(cut -f2- -d: <<< $p)
+        export GOROOT=$goSourcePath
+        unset IFS
+        unset dedupe
+        unset newpath
+    fi
+}
