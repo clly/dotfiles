0e5da818c303803adbd78b29a558c3fee0bd53e5
Connor Kelly <connor.r.kelly@gmail.com>
2019-08-08 12:02:25 -0500

    Add some kube aliases and git prompt

--- alias/kube.sh
+++ alias/kube.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
 alias kcd='kubectl config set-context $(kubectl config current-context) --namespace '
+alias kcu='kubectl config use-context '
+alias kc='kubectl config get-contexts'
+
+function minikube_svc() {
+    local svc=$1
+    if [[ -z $svc ]]; then
+        echo "Empty service"
+        return 1
+    fi
+
+    kubectl get svc $svc -o json | jq -r .spec.clusterIP
+}
