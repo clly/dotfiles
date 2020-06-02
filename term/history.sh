096b06de41cacb7ea6c854c6404be9690e22efc7
Connor Kelly <connor.r.kelly@gmail.com>
2020-06-02 14:56:21 -0500

    Still messing with history

--- term/history.sh
+++ term/history.sh
@@ -3,6 +3,6 @@
 # append to history file. Allows history to be available across multiple sessions
 export HISTCONTROL=ignorespace:ignoredups:erasedups
 shopt -s histappend
-export HISTSIZE=1000000
-export HISTFILESIZE=1000000
-export PROMPT_COMMAND="history -n;history -w"
+export HISTSIZE=1000
+export HISTFILESIZE=2000
+export PROMPT_COMMAND="history -a;history -r"
