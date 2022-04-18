95b04befca6ee47460630a7a2ca757989e3e9420
Connor Kelly <connor.r.kelly@gmail.com>
2022-04-17 20:58:24 -0500

    Upgrade go version

--- varz.sh
+++ varz.sh
@@ -2,8 +2,8 @@
 
 
 source "${DOT_SOURCE}/os.bash"
-export GIT_EDITOR="vim"
-export GOVERSION="1.17.1.${ARCH}"
+export GIT_EDITOR="nvim"
+export GOVERSION="1.18.1.${ARCH}"
 export PROJECT_DIR="${HOME}/p"
 export WPROJECT_DIR="${HOME}/w"
 export DOCKER_PROJECT_DIR="${HOME}/d"
